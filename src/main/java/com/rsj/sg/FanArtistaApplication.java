9390836e91cd055a5550af9009833537153bd959
JjersaR <rikisj5649@gmail.com>
2026-01-30 10:06:47 -0600

    first commit

---
+++ src/main/java/com/rsj/sg/FanArtistaApplication.java
@@ -0,0 +1,13 @@
+package com.rsj.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanArtistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanArtistaApplication.class, args);
+	}
+
+}
